c8471eacc885bafb16bbb4c29e15b3ce9a8d2cbe
Peter Brink <peterg@varpro.org>
2019-03-22 07:10:51 -0400

    more inital updates

--- index.js
+++ index.js
@@ -1,16 +1,82 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const token = process.env.DISCORD_SECRET;
+require('dotenv').config()
+const { Client: Bot, RichEmbed } = require('discord.js');
+const Tail = require('tail').Tail;
+var Rcon = require('simple-rcon');
 
-client.on('ready', () => {
+
+// Factorio Direct Connect through RCON
+const {
+  DISCORD_SECRET_TOKEN,
+  DISCORD_FACTORIO_CHANNEL_ID
+} = process.env
+
+var factorio = new Rcon({
+    host: 'localhost',
+    port: instanceconfig.clientPort, // port factorio listens after RCON connections on
+    password: instanceconfig.clientPassword, // RCON password
+    timeout: 0
+});
+setTimeout(() => { factorio.connect(); }, 5000);
+ 
+factorio.on('authenticated', function() {
+    console.log('Authenticated!');
+}).on('connected', function() {
+    console.log('Connected!');
+}).on('disconnected', function() {
+    console.log('Disconnected!');
+    // now reconnect
+    factorio.connect();
+});
+// Now do whatever you want!
+// silent-command doesn't show up in chat, nice to avoid spam :)
+//factorio.exec("/silent-command remote.call('clusterio', 'doStuff', 'Some data of some kind')");
+
+
+
+// Discord Bot Setup
+const bot = new Bot();
+
+const {
+  DISCORD_SECRET_TOKEN,
+  DISCORD_FACTORIO_CHANNEL_ID
+} = process.env
+
+bot.on('ready', () => {
   console.log("I'm in");
-  console.log(client.user.username);
+  console.log(bot.user.username);
 });
 
-client.on('message', msg => {
-    if (msg.author.id != client.user.id) {
+bot.on('message', msg => {
+  if (msg.author.id != bot.user.id) {
+    console.log('got a new message from someone else');
+    if (msg.content.includes('paul')) {
+      msg.channel.send('I heard she is so fineeeee');
+    } else {
         msg.channel.send(msg.content.split('').reverse().join(''));
     }
+  }
 });
 
-client.login(token);
+bot.on('disconnected', function() {
+  // Relog again here
+  console.log('trying to relogin');
+});
+
+bot.on('error', error => {
+  console.log('discord bot error: ', error);
+  console.log('current bot: ', bot);
+});
+
+bot.login(DISCORD_SECRET_TOKEN);
+
+
+// Reading log file
+var tail = new Tail("C:/Users/User/Documents/writetesting.txt");
+tail.watch()
+tail.on("line", data => {
+  if (data.includes('[CHAT]') || data.includes('[LEAVE]') || data.includes('[JOIN]')) {
+    bot.channels.get(DISCORD_FACTORIO_CHANNEL_ID).send(data);
+  }
+});
+
+console.log('done')
